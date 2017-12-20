ed54b4850b370df527966f367c834e142f2a8e2e
renchuan521 <32608218+renchuan521@users.noreply.github.com>
2017-12-20 11:36:36 +0800

    Update db.php

--- config/db.php
+++ config/db.php
@@ -2,17 +2,12 @@
 
 return [
     'db'=>[
-        'host' => 'rm-uf652a57e1pa150bpo.mysql.rds.aliyuncs.com',
+        'host' => 'localhost',
         'user'=>'root',
-        'password'=>'123+++ao',
-        'dbname'=>'mvc',
+        'password'=>'',
+        'dbname'=>'',
         'port'=>3306,
         'prefix'=>'',
         'charset'=>'utf8',
-    ],
-    'data'=>[
-        'name'=>'renchuan',
-        'sex'=>'男',
-        'age'=>'11',
     ],
 ];
